51b2647feeb2ebf55819ab5300bb152b3ee3d8c2
somT-oss <somtochukwuuchegbu@gmail.com>
2025-10-21 13:13:45 +0100

    add: database

--- books-backend/app/database.py
+++ books-backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = ""
+DATABASE_URL = "sqlite:///./books.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
